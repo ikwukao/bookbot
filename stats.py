a39c90740db7f4205424811bfa62dc66334ccdd6
Ikwuka Okoye <ikwukao@proton.me>
2026-07-03 18:32:42 +0100

    chore: populate get_num_words function and get_num_chars function

--- stats.py
+++ stats.py
@@ -1,2 +1,15 @@
-def get_num_words():
-    pass
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+
+def get_num_chars(text):
+    char_count = {}
+
+    for char in text.lower():
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+
+    return char_count
