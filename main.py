47dc0b0f2badad256be0d204c71c9a47793d7816
Ikwuka Okoye <ikwukao@proton.me>
2026-07-03 17:42:51 +0100

    feat(main.py): count number of word in the book

--- main.py
+++ main.py
@@ -7,3 +7,12 @@ def main():
     print(book_text)
 
 main()
+
+
+# Count number of words
+with open("books/frankenstein.txt", "r", encoding="utf-8") as file:
+    content = file.read()
+    words = content.split()
+    word_count = len(words)
+
+    print(f"Found {word_count} total words")
