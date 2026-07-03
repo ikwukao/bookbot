0f67c97b16e8aa46b769f5a0378ef70497cee43b
Ikwuka Okoye <ikwukao@proton.me>
2026-07-03 17:43:45 +0100

    chore: reformat main.py with ruff

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ def get_book_text(filepath):
     with open(filepath, "r") as file:
         return file.read()
 
+
 def main():
     book_text = get_book_text("books/frankenstein.txt")
     print(book_text)
+
 
 main()
 
