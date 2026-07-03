ee08d3b55cd78fcc26cde5749e3009ead6f37fc0
Ikwuka Okoye <ikwukao@proton.me>
2026-07-03 18:33:09 +0100

    feat: refactor main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from stats import get_num_words
+from stats import get_num_words, get_num_chars
+
 
 def get_book_text(filepath):
     with open(filepath, "r") as file:
@@ -6,17 +7,22 @@ def get_book_text(filepath):
 
 
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
-    print(book_text)
+    text = get_book_text("books/frankenstein.txt")
+
+    num_words = get_num_words(text)
+    print(f"{num_words} words found in the document")
+
+    char_count = get_num_chars(text)
+    print(char_count)
 
 
 main()
 
 
-# Count number of words
+# Read number of words in a file
 with open("books/frankenstein.txt", "r", encoding="utf-8") as file:
     content = file.read()
     words = content.split()
     word_count = len(words)
 
-    print(f"Found {word_count} total words")
+print(f"Found {word_count} total words")
